b0f60d8a384aed7eb39bf6376b3f6a9ec4aaeb6c
zaidalasfar97 <z.alasfar@hotmail.com>
2021-04-03 21:55:46 +0300

    done

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,36 @@
 'use strict';
-
-// Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../linked-list');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+
+  let listTest = new LinkedList();
+
+  it('Should be empty linked list', () => {
+    expect(listTest.head).toEqual(null);
+  });
+
+
+  it('Should insert new node to linked list', () => {
+    listTest.insert(12);
+    expect(listTest.head.value).toBe(12);
+  });
+
+
+  it('Should insert new value to linked list', () => {
+    listTest.insert(20);
+    expect(listTest.head.value).toBe(20);
+  });
+
+
+  it('Should find a value anywhere in the linked list', () => {
+    listTest.insert(22);
+    expect(listTest.includes(22)).toBeTruthy();
+  });
+
+
+  it('should convert to string', () => {
+    let emptyList = new LinkedList();
+    expect(listTest.toString()).toBe('{ 22 } -> {20} -> {12} -> NULL');
+    expect(emptyList.toString()).toBe('Null');
   });
 });
