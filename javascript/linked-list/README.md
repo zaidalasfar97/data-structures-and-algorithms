f64d304c2c634734d49751cb2fa76e7310d02040
zaidalasfar97 <z.alasfar@hotmail.com>
2021-04-04 23:41:01 +0300

    finish

--- javascript/linked-list/README.md
+++ javascript/linked-list/README.md
@@ -2,7 +2,7 @@
 ### Create methods for singly linked list . 
 
 ## Challenge : 
-#### To create a Linked List class that have three methods insert , includes , and to string .
+#### To create a Linked List class that have three methods insert , includes , and to string , append , insert before , insert after . 
 
 
 ## Approach & Efficiency :
@@ -16,9 +16,19 @@
 ### tostring : Take an arguments and returns a sting replace all values in linked list formatted as : "{ a } -> { b } -> { c } -> NULL"
 ### The big O : space O(1)|| time O(n) 
 
+### Append : which adds a new node with the given value to the end of the list
+
+### insert before : which add a new node with the given newValue immediately before the first value node.
+
+
+### insert after : which add a new node with the given newValue immediately after the first value node 
+
 
 # API : 
 ### We have three method : 
 #### Insert : Accept a node as an argument and insert the node to first of linked list .
 #### Includes : Accept a node as an argument and return boolean value , return true if the value exist or false if not exist . 
 #### Tostring : Accept no argument and return a string representing all values in linked list like : "{ a } -> { b } -> { c } -> NULL"
+
+
+![Solution](./img/linked-list.png)
--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,73 +1,47 @@
 'use strict';
-const Node = require('../linked-list').Node;
-const LinkedList = require('../linked-list').LinkedList;
 
-describe('Linked List test', () => {
+const { expect } = require('@jest/globals');
+const LL = require('../linked-list');
 
-  it('Should be empty', () => {
-    let linkedList = new LinkedList();
-    expect(linkedList.head).toEqual(null);
+describe('Linked List', () => {
+  it('Constructor()', () => {
+    const list = new LL();
+    expect(list.head).toBeNull();
   });
 
+  it('insert()', () => {
+    const list = new LL();
+    const initValue = 6;
+    list.insert(initValue);
+    expect(list.head.value).toEqual(initValue);
+    const newValue = 7;
+    list.insert(newValue);
+    expect(list.head.value).toEqual(newValue);
+    expect(list.head.next.value).toEqual(initValue);
 
-  it('Should add head to linked-list', () => {
-    let ll = new LinkedList();
-    ll.insert(12);
-    ll.insert(3);
+    let currentNode = list.head;
 
-    expect(ll.head.value).toEqual(3);
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    expect(currentNode.next).toBeNull();
   });
+  it('include()', () => {
+    const list = new LL();
+    const initValue = 6;
+    list.insert(initValue);
 
-
-
-  it('Should add new node to linked-list', () => {
-    let ll = new LinkedList();
-    let val = 22;
-    ll.insert(val);
-
-    expect(ll.head.value).toEqual(val);
-    expect(ll.head.next).toBeNull();
+    expect(list.includes(initValue)).toBeTruthy();
+    expect(list.includes(150)).toBeFalsy();
   });
+  it('toString()', () => {
+    const list = new LL();
+    const initValue = 6;
+    const newValue = 7;
+    list.insert(initValue);
+    list.insert(newValue);
+    console.log(list.toString());
 
-
-
-  it('should add new header to linked list', () => {
-
-    let ll = new LinkedList();
-    ll.insert(33);
-    let val = 55;
-    ll.insert(val);
-
-    expect(ll.head.value).toEqual(val);
-    expect(ll.head.next.value).toEqual(33);
-  });
-
-
-
-  it('Should insert multiple Nodes', () => {
-    let ll = new LinkedList();
-    ll.insert(1);
-    ll.insert(2);
-
-    expect(ll.length).toEqual(2);
-  });
-
-
-
-  it('Should return true if find a value else return false', () => {
-    let ll = new LinkedList();
-    ll.insert(2);
-
-    expect(ll.includes(2)).toEqual(true);
-    expect(ll.includes(2000)).toEqual(false);
-  });
-
-
-
-  it('Should return all values in ll as a string', () => {
-    let ll = new LinkedList();
-    ll.insert(1);
-    ll.insert(2);
-    expect(ll.toString()).toEqual("{2} -> {1} -> Null");
+    expect(list.toString()).toEqual(`{ 7 } -> { 6 } -> NULL`);
   });
 });
